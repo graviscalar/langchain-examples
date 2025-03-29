46e0044a39630f1348a52308793c920f322a6eaf
graviscalar <nebula_m422000@yahoo.com>
2025-03-29 21:46:45 +0000

    Ollama and Router with RunnableLambda/RunnableBranch

---
+++ ollama/ollama_router_runnablelambda.py
@@ -0,0 +1,89 @@
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+if __name__ == "__main__":
+    # use your Ollama model. Verify loaded models with 'ollama list'
+    model = "gemma3:4b"
+    temperature = 0
+
+    llm_ollama = ChatOllama(model=model,
+                            temperature=temperature)
+
+    # Setup chain for topic
+    topic_chain = (
+            PromptTemplate.from_template(
+            """Given the user question below, classify it as either being about `Python`, `Pytorch`, or `Scikit-learn`.
+          
+              Do not respond with more than one word.
+          
+              <question>
+              {question}
+              </question>
+          
+              Classification:"""
+            )
+            | llm_ollama
+            | StrOutputParser()
+    )
+
+    # Setup sub-chains
+
+    # Python chain
+    python_chain = PromptTemplate.from_template(
+        """You are an expert in Python. \
+    Always answer questions starting with "As Python expert". \
+    Respond to the following question:
+
+    Question: {question}
+    Answer:"""
+    ) | llm_ollama
+
+    # Pytorch chain
+    pytorch_chain = PromptTemplate.from_template(
+        """You are an expert in Pytorch. \
+    Always answer questions starting with "As Pytorch expert". \
+    Respond to the following question:
+
+    Question: {question}
+    Answer:"""
+    ) | llm_ollama
+
+    # Scikit-learn chain
+    scikit_chain = PromptTemplate.from_template(
+        """You are an expert in Scikit-learn. \
+    Always answer questions starting with "As Scikit-learn expert". \
+    Respond to the following question:
+
+    Question: {question}
+    Answer:"""
+    ) | llm_ollama
+
+    # Select proper chain
+    def route_select(info):
+        if "python" in info["topic"].lower():
+            return python_chain
+        elif "pytorch" in info["topic"].lower():
+            return pytorch_chain
+        elif "scikit-learn" in info["topic"].lower():
+            return scikit_chain
+        else:
+            return scikit_chain
+
+    # Create a final chain
+    final_chain = {"topic": topic_chain, "question": lambda x: x["question"]} | RunnableLambda(
+        route_select
+    )
+
+    # Route to Python chain
+    result = final_chain.invoke({"question": "tell me about decorators"})
+    print(f"Routing to Python chain ----------------------------------------------------------------------\n{result}\n")
+
+    # Route to Pytorch chain
+    result = final_chain.invoke({"question": "tell me about nn.Module"})
+    print(f"Routing to Pytorch chain ---------------------------------------------------------------------\n{result}\n")
+
+    # Route to Scikit-learn chain
+    result = final_chain.invoke({"question": "tell me about random forest"})
+    print(f"Routing to Scikit-learn chain ----------------------------------------------------------------\n{result}\n")
